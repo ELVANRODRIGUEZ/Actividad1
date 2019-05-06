327c9fbe9d403f32b3d1e5622f4fad567b1aa816
ELVANRODRIGUEZ <elvanovich2000@yahoo.com.mx>
2019-05-06 01:52:48 -0500

    Look to the 'Project' page finished. Functionality for the video play-pause and project slides navigation added.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,8 +1,78 @@
 $(document).ready(function () {
 
+    // ============================================== PROJECTS
 
-    // ============================================== FIREBASE
+    // ==================================== VIDEO CONTROL
 
-    
+    var $video = $("#shownVideo").get(0);
+    var $playIcon = $("#playPauseIcon");
+    var playIconToggler = true;
+
+    $video.loop = true;
+
+    $playIcon.on("click", function () {
+        if (playIconToggler) {
+            playVideo();
+
+        }
+    })
+
+    $("#shownVideo").on("click", function () {
+        if (playIconToggler) {
+            playVideo();
+        } else {
+            pauseVideo();
+        }
+    })
+
+    function playVideo() {
+        $playIcon.css("visibility", "hidden");
+        $video.play();
+        playIconToggler = false;
+    }
+
+    function pauseVideo() {
+        $playIcon.css("visibility", "visible");
+        $video.pause();
+        playIconToggler = true;
+    }
+
+
+    // ==================================== PROJECT NAVIGATION
+
+    var slideCounter = 0;
+    var previous = $("#prev");
+    var next = $("#next");
+
+
+    next.on("click", function () {
+        if (slideCounter == 0) {
+            previous.css("opacity", "1");
+
+        }
+        if (slideCounter < 5) {
+            slideCounter++;
+
+        } else if (slideCounter == 5 ) {
+            slideCounter++;
+            $(this).css("opacity", ".25");
+        }
+        console.log(slideCounter);
+    });
+
+    previous.on("click", function() {
+        if (slideCounter == 6) {
+            next.css("opacity", "1");
+            slideCounter--;
+
+        } else if (slideCounter == 1) {
+            $(this).css("opacity", ".25");
+            slideCounter--;
+
+        } else if (slideCounter > 0 ) {
+            slideCounter--;
+        }
+        console.log(slideCounter);
+    });
 
 });
